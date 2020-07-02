703094e04033d4a80546141d86d4d6c7aab49e01
agilancolbert <35196337+agilancolbert@users.noreply.github.com>
2020-07-02 18:06:40 +0200

    Update DeployInfra.tf

--- DeployInfra.tf
+++ DeployInfra.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devops-efrei-agikexel"
-    key    = "WebApache/${var.env}/DeployInfra/infra.tfstate"
+    key    = "WebApache/dev/DeployInfra/infra.tfstate"
     region = "us-west-1"
   }
 }
