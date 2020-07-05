3fd53bd652503d75f2428939b94941db75c55366
agilancolbert <35196337+agilancolbert@users.noreply.github.com>
2020-07-05 22:31:48 +0200

    Update DeployInfra.tf

--- DeployInfra.tf
+++ DeployInfra.tf
@@ -193,3 +193,18 @@ resource "aws_route_table_association" "public-3" {
   subnet_id      = aws_subnet.subnet-public-3.id
   route_table_id = aws_route_table.public.id
 }
+
+# Test Base RDS
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "azerty1234"
+  parameter_group_name = "default.mysql5.7"
+  publicly_accessible = true
+}
