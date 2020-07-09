725b9f0fce56cdb3c2cfccd272d7ba757f144e25
agilancolbert <35196337+agilancolbert@users.noreply.github.com>
2020-07-09 11:14:31 +0200

    Update DeployInfra.tf

--- DeployInfra.tf
+++ DeployInfra.tf
@@ -9,10 +9,10 @@ provider "aws" {
 
 # VPC
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
+  cidr_block           = var.cidr_block_vpc
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_classiclink   = var.enable_classiclink
   instance_tenancy     = "default"
 
   tags = {
@@ -34,9 +34,9 @@ resource "aws_internet_gateway" "igw" {
 ### AZ1
 resource "aws_subnet" "subnet-public-1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1a"
+  cidr_block              = var.cidr_block_spb1
+  map_public_ip_on_launch = var.map_public_ip_on_launch_spb1
+  availability_zone       = var.availability_zone_spb1
   tags = {
     Name = "${var.env}-subnet-public-1"
   }
@@ -45,9 +45,9 @@ resource "aws_subnet" "subnet-public-1" {
 ### AZ2
 resource "aws_subnet" "subnet-public-2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1b"
+  cidr_block              = var.cidr_block_spb2
+  map_public_ip_on_launch = var.map_public_ip_on_launch_spb2
+  availability_zone       = var.availability_zone_spb2
   tags = {
     Name = "${var.env}-subnet-public-2"
   }
@@ -56,9 +56,9 @@ resource "aws_subnet" "subnet-public-2" {
 ### AZ3
 resource "aws_subnet" "subnet-public-3" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1c"
+  cidr_block              = var.cidr_block_spb3
+  map_public_ip_on_launch = var.map_public_ip_on_launch_spb3
+  availability_zone       = var.availability_zone_spb3
   tags = {
     Name = "${var.env}-subnet-public-3"
   }
@@ -68,9 +68,9 @@ resource "aws_subnet" "subnet-public-3" {
 ### AZ1
 resource "aws_subnet" "subnet-private-1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.4.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1a"
+  cidr_block              = var.cidr_block_spr1
+  map_public_ip_on_launch = var.map_public_ip_on_launch_spr1
+  availability_zone       = var.availability_zone_spr1
   tags = {
     Name = "${var.env}-subnet-private-1"
   }
@@ -79,9 +79,9 @@ resource "aws_subnet" "subnet-private-1" {
 ### AZ2
 resource "aws_subnet" "subnet-private-2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.5.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1b"
+  cidr_block              = var.cidr_block_spr2
+  map_public_ip_on_launch = var.map_public_ip_on_launch_spr2
+  availability_zone       = var.availability_zone_spr2
   tags = {
     Name = "${var.env}-subnet-private-2"
   }
@@ -90,9 +90,9 @@ resource "aws_subnet" "subnet-private-2" {
 ### AZ3
 resource "aws_subnet" "subnet-private-3" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.6.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1c"
+  cidr_block              = var.cidr_block_spr3
+  map_public_ip_on_launch = var.map_public_ip_on_launch_spr3
+  availability_zone       = var.availability_zone_spr3
   tags = {
     Name = "${var.env}-subnet-private-3"
   }
@@ -100,11 +100,11 @@ resource "aws_subnet" "subnet-private-3" {
 
 # Nat Instance
 resource "aws_instance" "nat" {
-  ami                    = "ami-0f630a3f40b1eb0b8"
+  ami                    = var.ami
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet-public-1.id
   vpc_security_group_ids = [aws_security_group.allow_nat.id]
-  source_dest_check      = "false"
+  source_dest_check      = var.source_dest_check
 
   user_data = <<-EOF
         #!/bin/bash
@@ -119,28 +119,28 @@ resource "aws_instance" "nat" {
 
 # Nat SG
 resource "aws_security_group" "allow_nat" {
-  name        = "allow_web"
+  name        = var.name
   vpc_id      = aws_vpc.vpc.id
   description = "Allow inbound traffic"
 }
 
 ## SG Rule egress
 resource "aws_security_group_rule" "web_egress_allow_all" {
-  type              = "egress"
-  to_port           = 0
-  protocol          = "-1"
-  from_port         = 0
-  cidr_blocks       = ["0.0.0.0/0"]
+  type              = var.web_egress_allow_all.type
+  to_port           = var.web_egress_allow_all.to_port
+  protocol          = var.web_egress_allow_all.protocol
+  from_port         = var.web_egress_allow_all.from_port
+  cidr_blocks       = var.web_egress_allow_all.cidr_blocks
   security_group_id = aws_security_group.allow_nat.id
 }
 
 ## SG Rule ingress
 resource "aws_security_group_rule" "ingress_allow_private" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  type              = var.ingress_allow_private.type
+  from_port         = var.ingress_allow_private.from_port
+  to_port           = var.ingress_allow_private.to_port
+  protocol          = var.ingress_allow_private.protocol
+  cidr_blocks       = var.ingress_allow_private.cidr_blocks
   security_group_id = aws_security_group.allow_nat.id
 }
 
@@ -151,7 +151,7 @@ resource "aws_default_route_table" "main-private" {
   default_route_table_id = aws_vpc.vpc.default_route_table_id
 
   route {
-    cidr_block  = "0.0.0.0/0"
+    cidr_block  = var.cidr_block_main_pri
     instance_id = aws_instance.nat.id
   }
 
@@ -165,7 +165,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.cidr_block_pub
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -189,4 +189,3 @@ resource "aws_route_table_association" "public-3" {
   subnet_id      = aws_subnet.subnet-public-3.id
   route_table_id = aws_route_table.public.id
 }
-
